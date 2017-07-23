c20ed75bba3d2c51fce44fd73d0feeea46c354d2
KensoDev <avi@kensodev.com>
2017-07-22 20:09:34 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { syncHistoryWithStore } from 'react-router-redux';
+import configureStore from './store';
+import routes from './routes';
+
+const initialState = {};
+const store = configureStore(initialState);
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      {routes}
+    </BrowserRouter>
+  </Provider>,
+  rootElement
+);
+
+registerServiceWorker();
---
+++ src/store.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { routerMiddleware, routerReducer as routing, push } from 'react-router-redux';
+import thunk from 'redux-thunk';
+
+const actionCreators = {
+  push
+};
+
+const reducers = {
+  routing
+};
+
+const middlewares = [ thunk ];
+
+const composeEnhancers = (() => {
+  const compose_ = window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  if(process.env.NODE_ENV === 'development' && compose_) {
+    return compose_({ actionCreators });
+  }
+  return compose;
+})();
+
+export default function configureStore(initialState) {
+  const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+  const rootReducer = combineReducers(reducers);
+
+  return createStore(rootReducer, initialState, enhancer);
+}
